3c1e80202c02c9743d6204a5a639b181259c954f
satishrg001 <satishrg001@gmail.com>
2024-07-31 19:23:54 +0530

    Added Configurable Parameters file.

---
+++ Parameters.py
@@ -0,0 +1,24 @@
+# Configurable Parameters
+
+logfile_path = "D:/exercise/"
+prj_folder = "D:/exercise/"
+prj_name = "hockey_stats_webpages/"
+html_site_folder = "www.scrapethissite.com/pages/forms/"
+p_num_prefix = "page_num_"
+file_extension = ".html"
+url = 'https://www.scrapethissite.com/pages/forms/'
+root_dir = "D:\exercise\hockey_stats_webpages"
+archive_path = "D:\exercise\html_webpages_"
+excel_file_name = "D:\exercise\hockey_processed_data_"
+excel_file_name_wl = "D:\exercise\winners_losers_"
+sheet_name1 = "NHL Stats 1990-2011"
+sheet_name2 = "Winner and Loser per Year"
+excel_file_extension = ".xlsx"
+warning_switch = "ignore"
+year_1 = 1990
+year_2 = 2011
+column_name_cmp = "Year"
+group_by_column_1 = "Year"
+group_by_column_2 = "Team Name"
+sum_column_1 = "Wins"
+sum_column_2 = "Losses"
